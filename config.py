9ee4a7e2ad99c2a511459e45228b61d7f2c897d3
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-05-22 12:34:48 +0800

    Use env vars for database and secret keys.

--- config.py
+++ config.py
@@ -6,10 +6,9 @@ basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-SECRET_KEY = 'some-secret-string'
-JWT_SECRET_KEY = 'jwt-secret-string'
+SECRET_KEY = environ.get('SECRET_KEY', 'some-secret-string')
+JWT_SECRET_KEY = environ.get('JWT_SECRET_KEY', 'jwt-secret-string')
 JWT_BLACKLIST_ENABLED = True
 JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']

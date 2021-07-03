6264fddca6a356f54b6e96226836bfe3c5fc064c
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-07-03 12:56:40 +0800

    Send time created info for user data.

--- models.py
+++ models.py
@@ -11,6 +11,7 @@ class User(db.Model):
     user_id: int
     username: str
     bio: str
+    time_created: datetime
 
     user_id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(128), unique = True, nullable = False)
